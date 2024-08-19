e2e2f08cef69503d5530b7d2009d4906861ab08a
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2024-08-19 15:29:49 +0200

    [client] Update mocked modules

--- packages/client/src/__mocks__/@perseid/core.ts
+++ packages/client/src/__mocks__/@perseid/core.ts
@@ -10,8 +10,16 @@
  * `@perseid/core` mock.
  */
 
+export class HttpClient {
+  protected mock = vi.fn();
+
+  protected async request(): Promise<void> {
+    await this.mock();
+  }
+}
 export class Model { protected mocked = vi.fn(); }
-export class Id { public toString = vi.fn(() => '123456789012345678901234'); }
+export class Logger { protected mocked = vi.fn(); }
+export class Id { public toString = vi.fn(() => '000000000000000000000001'); }
 export const toSnakeCase = vi.fn((variable: string) => `TO_SNAKE_CASE_${variable}`);
 export const deepMerge = vi.fn((_: unknown, variable: unknown): unknown => variable);
 export const deepCopy = vi.fn((variable: unknown): unknown => (typeof variable === 'object' ? { ...variable } : variable));
