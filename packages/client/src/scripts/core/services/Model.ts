4b17e9460adf8494a1209d3ee93e9286b9282ec6
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2024-10-12 13:24:48 +0200

    [client] Correctly deserialize data model schema fragments received from the API

--- packages/client/src/scripts/core/services/Model.ts
+++ packages/client/src/scripts/core/services/Model.ts
@@ -6,7 +6,13 @@
  *
  */
 
-import { deepMerge, type DefaultDataModel, Model as BaseModel } from '@perseid/core';
+import {
+  Id,
+  type FieldSchema,
+  Model as BaseModel,
+  type ResourceSchema,
+  type DefaultDataModel,
+} from '@perseid/core';
 
 /**
  * Data model.
@@ -18,12 +24,51 @@ export default class Model<
   DataModel extends DefaultDataModel = DefaultDataModel,
 > extends BaseModel<DataModel> {
   /**
+   * Deserializes `schema` received from the API.
+   *
+   * @param schema Serialized schema.
+   *
+   * @returns Data model field schema.
+   */
+  private deserializeSchema(schema: Record<string, unknown>): FieldSchema<DataModel> {
+    const formattedSchema: Record<string, unknown> = { ...schema };
+    const { fields } = formattedSchema;
+    if (formattedSchema.pattern !== undefined) {
+      formattedSchema.pattern = new RegExp(formattedSchema.pattern as string);
+    }
+    if (formattedSchema.enum !== undefined && formattedSchema.type === 'date') {
+      formattedSchema.enum = (formattedSchema.enum as string[]).map((date) => new Date(date));
+    } else if (formattedSchema.enum !== undefined && formattedSchema.type === 'id') {
+      formattedSchema.enum = (formattedSchema.enum as string[]).map((id) => new Id(id));
+    }
+    if (formattedSchema.type === 'array') {
+      formattedSchema.fields = this.deserializeSchema(fields as Record<string, unknown>);
+    } else if (fields !== undefined) {
+      const objectFields = fields as Record<string, unknown>;
+      formattedSchema.fields = Object.keys(objectFields).reduce((subFields, key) => ({
+        ...subFields,
+        [key]: this.deserializeSchema(objectFields[key] as Record<string, unknown>),
+      }), {});
+    }
+    return formattedSchema as unknown as FieldSchema<DataModel>;
+  }
+
+  /**
    * Updates data model with `schemaFragment`.
    *
    * @param schemaFragment Fragment of data model schema. Contains a subset of resources schemas.
    */
   public update(schemaFragment: Partial<DataModel>): void {
-    // TODO format stuff like pattern, enum, ...
-    this.schema = deepMerge(this.schema, schemaFragment);
+    const resources = (Object.keys(schemaFragment) as (keyof DataModel)[]);
+    Object.assign(this.schema, resources.reduce((model, resource) => {
+      const resourceDataModel = (schemaFragment)[resource] as ResourceSchema<DataModel>;
+      return {
+        ...model,
+        [resource]: {
+          ...resourceDataModel,
+          fields: this.deserializeSchema(resourceDataModel.fields),
+        },
+      };
+    }, {}));
   }
 }
