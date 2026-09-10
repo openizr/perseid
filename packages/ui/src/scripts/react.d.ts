9cf89b3bfffa7dee58764401c35f3a69af5b72b5
Matthieu Jabbour <matthieu.jabbour@gmail.com>
2026-09-10 16:55:54 +0200

    wip

--- packages/ui/src/scripts/react.d.ts
+++ packages/ui/src/scripts/react.d.ts
@@ -82,3 +82,106 @@ declare module '@perseid/ui/react' {
    */
   export function UIFilePicker(props: UIFilePickerProps): JSX.Element;
 }
+
+/**
+ * Adds native autocomplete for `data-layout` attribute to all React elements.
+ */
+declare namespace React {
+  /**
+   * Perseid built-in breakpoint.
+   */
+  type Breakpoint = 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl';
+
+  /**
+   * Perseid built-in shorthand.
+   */
+  type Shorthand = (
+    'grid'
+    | 'flex'
+    | 'none'
+    | 'block'
+    | 'inline'
+    | 'inline-flex'
+    | 'inline-grid'
+    | 'flex-wrap'
+    | 'flex-nowrap'
+    | 'flex-wrap-rev'
+    | 'flex-row'
+    | 'flex-col'
+    | 'flex-col-rev'
+    | 'flex-row-rev'
+    | 'grid-row'
+    | 'grid-col'
+    | 'grid-dense'
+    | 'flex-none'
+    | 'flex-auto'
+    | 'justify-between'
+    | 'justify-start'
+    | 'justify-end'
+    | 'justify-stretch'
+    | 'justify-center'
+    | 'items-center'
+    | 'items-start'
+    | 'items-end'
+    | 'items-stretch'
+    | 'self-center'
+    | 'self-start'
+    | 'self-end'
+    | 'self-stretch'
+    | 'text-left'
+    | 'text-right'
+    | 'text-center'
+    | 'text-justify'
+    | 'w-full'
+    | 'h-full'
+    | 'col-1'
+    | 'col-2'
+    | 'col-3'
+    | 'col-4'
+    | 'col-5'
+    | 'col-6'
+    | 'col-7'
+    | 'col-8'
+    | 'col-9'
+    | 'col-10'
+    | 'col-11'
+    | 'col-12'
+    | 'cols-1'
+    | 'cols-2'
+    | 'cols-3'
+    | 'cols-4'
+    | 'cols-5'
+    | 'cols-6'
+    | 'cols-7'
+    | 'cols-8'
+    | 'cols-9'
+    | 'cols-10'
+    | 'cols-11'
+    | 'cols-12'
+    | 'vgap-0'
+    | 'vgap-1'
+    | 'vgap-2'
+    | 'vgap-3'
+    | 'vgap-4'
+    | 'vgap-5'
+    | 'vgap-6'
+    | 'vgap-7'
+    | 'hgap-0'
+    | 'hgap-1'
+    | 'hgap-2'
+    | 'hgap-3'
+    | 'hgap-4'
+    | 'hgap-5'
+    | 'hgap-6'
+    | 'hgap-7'
+  );
+
+  /**
+   * Any available Perseid built-in layout shorthand.
+   */
+  type LayoutValue = Shorthand | `${Breakpoint}:${Shorthand}`;
+
+  interface HTMLAttributes<T> extends AriaAttributes, DOMAttributes<T> {
+    'data-layout'?: LayoutValue | (string & Record<never, never>);
+  }
+}
